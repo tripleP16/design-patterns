810ca1c59a12fc0d137aac4919e44c5880745795
Pablo Perez <perez51160900@hotmail.com>
2026-01-01 12:55:44 +0100

    feat: add Computer Builder pattern example and remove QueryBuilder example

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,86 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+class Computer {
+    public cpu: string = 'cpu not defined';
+    public ram: string = 'ram not defined';
+    public storage: string = 'storage not defined';
+    public gpu?: string;
+
+    displayConfiguration() {
+        console.log(`Configuracion de la computadora
+            CPU: ${this.cpu}
+            RAM: ${this.ram}
+            Almacenamiento: ${this.storage}
+            GPU: ${this.gpu}
+        `)
+    }
+}
+
+
+class ComputerBuilder {
+
+    private computer : Computer;
+
+    constructor() {
+        this.computer = new Computer();
+    }
+
+
+    setCPU(cpu: string): ComputerBuilder {
+        this.computer.cpu = cpu;
+        return this;
+    }
+
+    setStorage(storage: string): ComputerBuilder {
+        this.computer.storage = storage;
+        return this;
+    }
+
+    setRAM(ram: string): ComputerBuilder {
+        this.computer.ram = ram;
+        return this;
+    }
+
+    setGPU(gpu?: string): ComputerBuilder {
+        this.computer.gpu = gpu;
+        return this;
+    }
+
+    build(){
+        return this.computer;
+    }
+}
+
+
+function main() {
+    const basicComputer = new ComputerBuilder()
+    .setCPU('M3')
+    .setGPU('Integrated')
+    .setRAM('16gb')
+    .setStorage('1tb')
+    .build();
+
+    console.log(`%cComputadora Basica: `, COLORS.yellow)
+    
+    basicComputer.displayConfiguration();
+
+
+
+
+    const gamingComputer = new ComputerBuilder()
+    .setCPU('Intel I9')
+    .setCPU('AMD ryzen 9')
+    .setRAM('32gb')
+    .setGPU('NVIDIA 4070')
+    .setStorage('3tb')
+    .build()
+
+    console.log(`%cComputadora Gaming: `, COLORS.yellow)
+    gamingComputer.displayConfiguration()
+}
+
+main();

0aad7924f459ef2b95cf874d13dcc1dc9785b53f
Pablo Perez <perez51160900@hotmail.com>
2026-01-05 21:12:35 +0100

    feat: implement SQL query builder pattern

--- 01-creacionales/01.2-builder.ts
+++ 01-creacionales/01.2-builder.ts
@@ -35,3 +35,64 @@ import { COLORS } from '../helpers/colors.ts';
   console.log('Consulta: ', usersQuery);
   // Select id, name, email from users where age > 18 and country = 'Cri' order by name ASC limit 10;
  */
+
+//! Solución
+
+class QueryBuilder {
+  private table: string;
+  private fields: string[] = [];
+  private conditions: string[] = [];
+  private orderFields: string[] = [];
+  private limitCount?: number;
+
+  constructor(table: string) {
+    this.table = table;
+  }
+
+  select(...fields: string[]): QueryBuilder {
+    this.fields = fields;
+    return this ;
+  }
+
+  where(condition: string): QueryBuilder {
+    this.conditions.push(condition);
+    return this;
+  }
+
+  orderBy(field: string, direction: 'ASC' | 'DESC' = 'ASC'): QueryBuilder {
+    this.orderFields.push(`${field} ${direction}`)
+    return this;
+    
+  }
+
+  limit(count: number): QueryBuilder {
+    this.limitCount = count;
+    return this;
+  }
+
+  execute(): string {
+    // Select id, name, email from users where age > 18 and country = 'Cri' order by name ASC limit 10;
+    const selectedFields = this.fields.length > 0 ? `SELECT ${this.fields.join(', ')}`: 'SELECT *';
+    const whereClasue = this.conditions.length > 0 ? `WHERE ${this.conditions.join(' AND ')}`: '';
+    const orderBy = this.orderFields.length > 0 ? `ORDER BY ${this.orderFields.join(' ,')}` : '';
+    const limitClause = this.limitCount != null ? `LIMIT ${this.limitCount}` : '';
+    
+
+   return `${selectedFields} FROM ${this.table} ${whereClasue} ${orderBy} ${limitClause};`
+  }
+}
+
+function main() {
+  const usersQuery = new QueryBuilder('users')
+    .select('id', 'name', 'email')
+    .where('age > 18')
+    .where("country = 'Cri'") // Esto debe de hacer una condición AND
+    .orderBy('name', 'ASC')
+    .limit(10)
+    .execute();
+
+  console.log('%cConsulta:\n', COLORS.red);
+  console.log(usersQuery);
+}
+
+main();
